908ea3919167ae7bf6b8f4d10324f7b4725c1d31
YOUNGHO0 <31pqopq13@gmail.com>
2024-09-21 20:37:58 +0900

    Feat : ffmpeg 관련 헤더/전처리 추가

--- myQtPlayer/main.cpp
+++ myQtPlayer/main.cpp
@@ -1,3 +1,17 @@
+extern "C"{
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libswresample/swresample.h>
+#include <libswscale/swscale.h>
+#include <libavutil/avutil.h>
+}
+
+#pragma comment(lib, "avformat.lib")
+#pragma comment(lib, "avcodec.lib")
+#pragma comment(lib, "swresample.lib")
+#pragma comment(lib, "swscale.lib")
+#pragma comment(lib, "avutil.lib")
+
 #include <QApplication>
 #include <QPushButton>
 
